aa0890cd7f68deb47635cd89a546d49f4a9e5fe5
Chandra Kiran <kallepallychandrakiran@gmail.com>
2022-07-18 15:41:58 +0530

    Fibonacci code has been added

---
+++ Java/Fibonacci.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Fibonacci {
+    void Fibonaccicalc(int z) {
+        int firstterm, secondterm, thirdterm;
+        firstterm = 0;
+        secondterm = 1;
+        thirdterm = firstterm + secondterm;
+
+    }
+}
